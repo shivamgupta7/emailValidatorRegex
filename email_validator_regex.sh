106cf93e281be5fe303a02bda5b1fb7c048dfd94
shivamg.cr7@gmail.com <shivamg.cr7@gmail.com>
2020-03-28 14:15:16 +0530

    [Shivam] Add optional part after 1st mandatory part.

--- email_validator_regex.sh
+++ email_validator_regex.sh
@@ -2,10 +2,10 @@
 shopt -s extglob
 echo "Enter Email Id :"
 read email
-email_pat3="^([a-zA-Z]+)@([a-zA-Z]+)\.([a-z]{2,4})$"
-if [[ $email =~ $email_pat3 ]]
+email_pat4="^([a-zA-Z]+[a-zA-Z0-9]*((\.|\-|\+|\_)?[A-Za-z0-9]*))@([a-zA-Z0-9]+)\.([a-z]{2,4})$"
+if [[ $email =~ $email_pat4 ]]
 then
-   echo "Email use case 3 is valid."
+   echo "Email use case 4 is valid."
 else
    echo "Please enter correct input."
 fi
