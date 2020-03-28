70ad5165851934b32422b376d55e8d1174f6fa2e
shivamg.cr7@gmail.com <shivamg.cr7@gmail.com>
2020-03-28 14:28:52 +0530

    [Shivam] Add last part is the optional country code and its 2 charaters only.

--- email_validator_regex.sh
+++ email_validator_regex.sh
@@ -2,10 +2,10 @@
 shopt -s extglob
 echo "Enter Email Id :"
 read email
-email_pat4="^([a-zA-Z]+[a-zA-Z0-9]*((\.|\-|\+|\_)?[A-Za-z0-9]*))@([a-zA-Z0-9]+)\.([a-z]{2,4})$"
-if [[ $email =~ $email_pat4 ]]
+email_pat5="^([a-zA-Z]+[a-zA-Z0-9]*((\.|\-|\+|\_)?[A-Za-z0-9]*))@([a-zA-Z0-9]+)\.([a-z]{2,4})(\.[a-z]{2})?$"
+if [[ $email =~ $email_pat5 ]]
 then
-   echo "Email use case 4 is valid."
+   echo "Email use case 5 is valid."
 else
    echo "Please enter correct input."
 fi
