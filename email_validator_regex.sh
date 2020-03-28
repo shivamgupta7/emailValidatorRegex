aab21586c5a20c896c83aa560b377b1b49d98227
shivamg.cr7@gmail.com <shivamg.cr7@gmail.com>
2020-03-28 13:05:59 +0530

    [Shivam] Ensure @ and validate the mandatory 2nd part.

--- email_validator_regex.sh
+++ email_validator_regex.sh
@@ -2,10 +2,10 @@
 shopt -s extglob
 echo "Enter Email Id :"
 read email
-email_pat1="^[a-zA-Z]+$"
-if [[ $email =~ $email_pat1 ]]
+email_pat2="^([a-zA-Z]+)@([a-zA-Z]+)$"
+if [[ $email =~ $email_pat2 ]]
 then
-   echo "Email use case 1 is valid."
+   echo "Email use case 2 is valid."
 else
    echo "Please enter correct input."
 fi
